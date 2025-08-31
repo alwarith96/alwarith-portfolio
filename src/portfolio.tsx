813c2643ac7611d31e60ec58387807fcd453820d
alwarith96 <alwarith1996@gmail.com>
2025-08-31 17:58:26 +0900

    Add files via upload

---
+++ src/portfolio.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Portfolio() {
+  return (
+    <div style={{textAlign:'center', padding:'2rem'}}>
+      <h1>Alwarith Portfolio</h1>
+      <p>Welcome to my portfolio website!</p>
+    </div>
+  )
+}
